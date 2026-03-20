e7d50b7fa4ccf838cc851ce97590d8f874f3bbbd
Parthka <hello@parthka.dev>
2026-03-20 20:27:54 +0530

    add things

--- ground/main.go
+++ ground/main.go
@@ -13,7 +13,10 @@ var sigAddr string = "46.62.235.81:9090"
 
 func main() {
 	room := os.Args[1]
-	findPeer("local/ip4", room+"_local_ip4")
+	go findPeer("local/ip4", room+"_local_ip4")
+	// go findPeer("local/ip6", room+"_local_ip6")
+	for {
+	}
 }
 
 func findPeer(peerType string, room string) {
@@ -26,8 +29,9 @@ func findPeer(peerType string, room string) {
 		panic(err)
 	}
 
-	peerAddr := getLocalPeerAddr(peerType)
-	peer, err := net.ListenUDP("udp", peerAddr)
+	peerAddr, peerNetwork := getLocalPeerAddr(peerType)
+	fmt.Println(peerAddr)
+	peer, err := net.ListenUDP(peerNetwork, peerAddr)
 	if err != nil {
 		fmt.Println("Error while create peer socket")
 		return
@@ -66,11 +70,11 @@ func findPeer(peerType string, room string) {
 				sig.Close()
 
 				// punch
-				punch(peer, otherPeer)
+				punch(peer, otherPeer, room)
 
 				break break_loop1
 			} else {
-				a, err := net.ResolveUDPAddr("udp", parts[1])
+				a, err := net.ResolveUDPAddr(peerNetwork, parts[1])
 				if err != nil {
 					fmt.Println("Invalid udp address", parts[1])
 					continue
@@ -91,36 +95,88 @@ func findAndSendAddrToSig(sig net.Conn, peer *net.UDPConn, peerType string) {
 		fmt.Println("peer loacl addr", peer.LocalAddr().String())
 		sigStr := fmt.Sprintf("sig/%s/\n", peer.LocalAddr().String())
 		sig.Write([]byte(sigStr))
-	case "local/ip6":
-		fmt.Println("peer loacl addr", peer.LocalAddr().String())
-		sigStr := fmt.Sprintf("sig/%s/\n", peer.LocalAddr().String())
-		sig.Write([]byte(sigStr))
 	case "remote/ip4":
 	case "remote/ip6":
 	}
 }
 
-func getLocalPeerAddr(peerType string) *net.UDPAddr {
+func getLocalPeerAddr(peerType string) (*net.UDPAddr, string) {
 	switch peerType {
-	case "local/ip4", "remote/ip4":
-		a, e := net.ResolveUDPAddr("udp", "0.0.0.0:0")
+	case "local/ip4":
+		ad := getLocalIp() + ":0"
+		a, e := net.ResolveUDPAddr("udp4", ad)
+		if e != nil {
+
+			fmt.Println("Error while resole addr")
+			panic(e)
+		}
+		return a, "udp4"
+	case "remote/ip4":
+		a, e := net.ResolveUDPAddr("udp4", ":0")
+		if e != nil {
+
+			fmt.Println("Error while resole addr")
+			panic(e)
+		}
+		return a, "udp4"
+	case "remote/ip6":
+		a, e := net.ResolveUDPAddr("udp", ":0")
 		if e != nil {
 			fmt.Println("Error while resole addr")
 			panic(e)
 		}
-		return a
-	case "local/ip6", "remote/ip6":
-		a, e := net.ResolveUDPAddr("udp", "[::]:0")
-		if e != nil {
-			fmt.Println("Error while resole addr")
-			panic(e)
-		}
-		return a
+		return a, "udp6"
 	default:
 		panic("invalid peer type")
 	}
 }
 
-func punch(peer *net.UDPConn, otherPeerAddr *net.UDPAddr) {
-	fmt.Println("Punch starting...")
+func punch(peer *net.UDPConn, otherPeerAddr *net.UDPAddr, room string) {
+	fmt.Println(peer.LocalAddr(), otherPeerAddr)
+	go func(peer *net.UDPConn) {
+		for {
+			peer.WriteToUDP([]byte("--PUNCH--: punch...ping..."), otherPeerAddr)
+			time.Sleep(400 * time.Millisecond)
+		}
+	}(peer)
+
+	r := make([]byte, 1024)
+
+	go func() {
+		//	punchd := false
+		for {
+			rl, add, err := peer.ReadFromUDP(r)
+			if err != nil {
+				continue
+			}
+			//	if otherPeerAddr.IP.Equal(add.IP) && otherPeerAddr.Port == add.Port && !punchd {
+			//	fmt.Println(room, "Punch Successfull")
+			//punchd = false
+			//}
+			//if !strings.HasPrefix(string(r[:rl]), "--PUNCH--:") {
+			fmt.Printf("%s: read \"%s\" from: %s\n", room, r[:rl], add.String())
+			//}
+		}
+	}()
+
+	var input string
+	for {
+		// fmt.Printf("Write to %s:", otherPeerAddr.String())
+		fmt.Scan(&input)
+		peer.WriteToUDP([]byte(input), otherPeerAddr)
+	}
+}
+
+func getLocalIp() string {
+	// Connect to an external address (the connection is never actually made)
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	defer conn.Close()
+
+	// Get the local address used for this simulated connection
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	return localAddr.IP.String()
 }
